b679d01151f5ebcc8021b99572395b15e66e0069
John Kong <JohN100x1@hotmail.co.uk>
2021-10-28 01:49:34 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Chessboard from './components/ChessBoard';
+
+function App() {
+  return (
+    <div>
+      <Chessboard/>
+    </div>
+  )
+};
+
+export default App;
